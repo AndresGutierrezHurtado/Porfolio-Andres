549a39c46a1d0827da4b14178bddafb995554a78
AndresGutierrezHurtado <152313655+AndresGutierrezHurtado@users.noreply.github.com>
2024-02-10 13:27:33 -0500

    Add files via upload

---
+++ PORFOLIO/index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/eb36e646d1.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/styles.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>Portafolio Andrés Gutiérrez Hurtado - Desarrollador y Programador Web </title>
+</head>
+<body class="bg-light">
+
+<nav class="navbar navbar-expand-sm fixed-top w-100 py-1">
+    <div class="container-sm justify-content-center">
+        <div class="mx-mdx-5">
+            <ul class="navbar-nav rounded-5 flex-row px-4">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#proyectos">Proyectos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#certificados">Certificados</a>
+                </li>
+                <li class="nav-item d-none d-sm-block">
+                    <a class="nav-link" href="#sobre-mi">Sobre mí</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main>
+    <section class="container container-personalizado py-5 px-3 px-sm-5 px-lg-0">
+        <div class="row">
+            <div class="col-12 col-md-9 my-3 my-sm-5">
+                <div class="d-flex grid gap-4 align-items-center">
+                    <img src="IMG/Foto-perfil.png" style="height: 70px; width: 70px; " class="rounded-circle border shadow">
+                    <div class="p-1 px-3 rounded-5 bg-success bg-opacity-25 border border-success text-success-emphasis scale" role="button">Disponible</div>
+                </div>
+                <h2 class="fs-1 fw-bold py-3">Hey, Soy Andrés Gutiérrez</h2>
+                <p style="text-wrap: balance; font-size: 18px;" class="pb-1"><strong style="color: rgb(68, 134, 221);">Técnico en Programación de Software</strong> con formación academica certificada en diversas tecnologias, 
+                    con <strong style="color: rgb(68, 134, 221);">dominio del idioma inglés nivel B1</strong> y en formación en Tecnólogo en Análisis y Desarrollo de Software.</p>
+                <div class="d-flex grid gap-4 row-gap-2  my-4 flex-column flex-sm-row">
+                    <a href="mailto:andres52885241@gmail.com" target="_blank" class="dark-hover p-1 px-3 rounded-5 border border-secondary-subtle"><i class="fa-regular fa-envelope"></i> Contáctame</a>
+                    <a href="https://www.linkedin.com/in/andres-gutiérrez-hurtado-25946728b/" target="_blank" class="dark-hover p-1 px-3 rounded-5 border border-secondary-subtle"><i class="fa-brands fa-linkedin"></i> Linkedin</a>
+                    <a href="Andres Gutierrez Hurtado CV.pdf" target="_blank" class="dark-hover p-1 px-3 rounded-5 border border-secondary-subtle"><i class="fa-regular fa-circle-down"></i> Descarga mi CV</a>
+
+                </div>
+            </div>
+        </div>
+        
+    </section>
+    <section id="proyectos" class="container container-personalizado pt-3 py-5 px-3 px-sm-5 px-lg-0">
+        <h1 class="fw-bold fs-2"><i class="fa-solid fa-code fs-4"></i>  Proyectos</h1>
+        <div class="py-4 d-flex flex-column grid gap-5">
+            <article class="d-flex row">
+                <div class="col-12 col-md-6 p1-1">
+                    <img src="IMG/Abejita22.png" class="w-100 h-100 rounded-4 border">
+                </div>
+                <div class="col-12 col-md-6">
+                    <h3 class="fw-bold py-2">Uniformes la Abejita 22</h3>
+                    <div class="d-flex grid gap-2 fs-7">
+                        <span class="border border-dark rounded-5 p-1 px-3 bg-dark text-light"><i class="fa-brands fa-bootstrap" style="color: purple;"></i>   Bootstrap CSS</span>
+                        <span class="border border-dark rounded-5 p-1 px-3 bg-dark text-light"><i class="fa-brands fa-js" style="color: yellow;"></i>  JQuery Js</span>
+                    </div>
+                    <p class="pt-3" style="text-wrap: balance;">
+                        Creador del sitio web de venta de uniformes escolares para la empresa La Abejita 22 ubicada en el barrio el ensueño.
+                    </p>
+                    <a href="https://laabejita22.000webhostapp.com" target="_blank" class="dark-hover p-1 px-4 rounded-5 border border-secondary-subtle"><i class="fa-solid fa-link"></i> Sitio Web</a>
+                </div>
+            </article>
+            <article class="d-flex row">
+                <div class="col-12 col-md-6 p1-1">
+                    <img src="IMG/Paradise-Cosmetics.png" class="w-100 h-100 rounded-4 border">
+                </div>
+                <div class="col-12 col-md-6">
+                    <h3 class="fw-bold py-2">Paradise Cosmetics</h3>
+                    <div class="d-flex grid gap-2 fs-7">
+                        <span class="border border-dark rounded-5 p-1 px-3 bg-dark text-light d-flex grid gap-2 align-items-center"><i class="fa-brands fa-bootstrap" style="color: purple;"></i>   Bootstrap CSS</span>
+                        <span class="border border-dark rounded-5 p-1 px-3 bg-dark text-light"><i class="fa-brands fa-js" style="color: yellow;"></i>  JQuery Js</span>
+                    </div>
+                    <p class="pt-3" style="text-wrap: balance;">
+                        Co-creador del proyecto Paradise Cosmetics, este es un sitio web de ventas de cosmeticos.
+                    </p>
+                    <a href="https://drive.google.com/drive/folders/19KhOzWyEQP1dq-M2y5Qy4fHMadj3GUHL?usp=sharing" target="_blank" class="dark-hover p-1 px-4 rounded-5 border border-secondary-subtle"><i class="fa-regular fa-eye"></i> Código</a>
+                </div>
+            </article>
+        </div>
+    </section>
+    <section class="container container-personalizado py-5 px-3 px-sm-5 px-lg-0" id="certificados">
+        <div class="card-wrapper">
+            <div class="row d-flex justify-content-center">
+                <div class="col-12">
+                    <h2 class="fw-bold"><i class="fa-solid fa-certificate fs-2"></i> Certificados</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi iusto aliquid error eum ab rerum natus 
+                        quia modi debitis nostrum officia minus, sit totam esse quisquam, impedit qui, suscipit reiciendis.</p>
+                </div>
+                <div class="col-9 col-sm-10 col-md-11 col-lg-12 position-relative">                    
+                    <button class="btn-r w-auto z-3 bg-white dark-hover rounded-circle fs-4 border position-absolute" style="height: 50px; width: 50px !important; top: 70px; right: -50px;"><i class="fa-solid fa-arrow-right"></i></button>
+                    <button class="btn-l w-auto z-3 bg-white dark-hover rounded-circle fs-4 border position-absolute" style="height: 50px; width: 50px !important; top: 70px; left: -50px;"><i class="fa-solid fa-arrow-left"></i></button>
+                    
+                    <div class="  d-flex grid gap-3 py-3 cartas-scroll" style="flex-wrap:nowrap; overflow-x: hidden;">                        
+                        <div class="col-lg-3">
+                            <div class="card rounded-3">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <p class="card-text m-0">Programación con JavaScript</p>
+                                        <p style="opacity: .75;" class="py-1 fst-italic mb-1">Movistar</p>
+                                    </div>
+                                    <a href="Certificados/Programacion-JS.pdf" target="_blank" class="dark-hover p-2 px-4 rounded-3 border border-secondary-subtle d-flex justify-content-center align-items-center grid gap-2"><i class="fa-regular fa-file"></i> Documento</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="card rounded-3">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <p class="card-text m-0">Fundamentos de programación</p>
+                                        <p style="opacity: .75;" class="py-1 fst-italic mb-1">Movistar</p>
+                                    </div>
+                                    <a href="Certificados/Fundamentos-programación.pdf" target="_blank" class="dark-hover p-2 px-4 rounded-3 border border-secondary-subtle d-flex justify-content-center align-items-center grid gap-2"><i class="fa-regular fa-file"></i> Documento</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="card rounded-3">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <p class="card-text m-0">Construcción de bases con MYSQL</p>
+                                        <p style="opacity: .75;" class="py-1 fst-italic mb-1">SENA</p>
+                                    </div>
+                                    <a href="Certificados/CBDMYSQL.pdf" target="_blank" class="dark-hover p-2 px-4 rounded-3 border border-secondary-subtle d-flex justify-content-center align-items-center grid gap-2"><i class="fa-regular fa-file"></i> Documento</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="card rounded-3">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <p class="card-text m-0">Desarrollo Web con PHP</p>
+                                        <p style="opacity: .75;" class="py-1 fst-italic mb-1">SENA</p>
+                                    </div>
+                                    <a href="Certificados/Desarrollo-php.pdf" target="_blank" class="dark-hover p-2 px-4 rounded-3 border border-secondary-subtle d-flex justify-content-center align-items-center grid gap-2"><i class="fa-regular fa-file"></i> Documento</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="card rounded-3">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <p class="card-text m-0">Variables y estructuras de control en Python</p>
+                                        <p style="opacity: .75;" class="py-1 fst-italic mb-1">SENA</p>
+                                    </div>
+                                    <a href="Certificados/VyE-PYTHON.pdf" target="_blank" class="dark-hover p-2 px-4 rounded-3 border border-secondary-subtle d-flex justify-content-center align-items-center grid gap-2"><i class="fa-regular fa-file"></i> Documento</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="card rounded-3">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <p class="card-text m-0">Variables y estructuras de control en Java</p>
+                                        <p style="opacity: .75;" class="py-1 fst-italic mb-1">SENA</p>
+                                    </div>
+                                    <a href="Certificados/VyE-Java.pdf" target="_blank" class="dark-hover p-2 px-4 rounded-3 border border-secondary-subtle d-flex justify-content-center align-items-center grid gap-2"><i class="fa-regular fa-file"></i> Documento</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="card rounded-3">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <p class="card-text m-0">Lenguaje de programación Python</p>
+                                        <p style="opacity: .75;" class="py-1 fst-italic mb-1">Universidad Distrital</p>
+                                    </div>
+                                    <a href="Certificados/VyE-Java.pdf" target="_blank" class="dark-hover p-2 px-4 rounded-3 border border-secondary-subtle d-flex justify-content-center align-items-center grid gap-2"><i class="fa-regular fa-file"></i> Documento</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container container-personalizado py-5 px-3 px-sm-5 px-lg-0" id="sobre-mi">
+        <h2 class="fw-bold"><i class="fa-solid fa-user-check fs-2"></i> Sobre mí</h2>
+        <article class="row">
+            <div class="col-12 col-md-8 py-3 d-flex flex-column grid gap-2">
+                <p class="m-0">Mi nombre es Andrés Gutiérrez, empecé en programación cuando conocí de este ámbito en el colegio hace 3 años. Actualmente <strong class="active">estoy estudiando en el programa ADSO del SENA</strong>.</p>
+                <p class="m-0">Uno de mis mayores logros en este campo, <strong class="active">es haber liderado el mejor proyecto de la ficha</strong>, y aunque hoy ya no sea muy utilizado, me ayudó mucho a aprender sobre desarrollo web.</p>
+                <p class="m-0">Mi objetivo es desarrollar aplicativos web de la <strong class="active">mayor calidad</strong>, y <strong class="active">mejorar todo lo posible en este campo</strong>.</p>
+            </div>
+            <div class="col-12 col-md-4 d-flex align-items-center justify-content-center">
+                <img class="h-auto w-auto mw-100 rounded "  src="IMG/Foto-2.png" alt="">
+            </div>
+            
+        </article>
+    </section>
+</main>
+<footer class="container container-personalizado py-5 px-3 px-sm-5 px-lg-0"> 
+    <div class="w-100 d-flex justify-content-between align-items-center bg-secondary bg-opacity-10 shadow-sm border rounded-5 px-3 p-1 fs-7 row">
+        <p class="col-12 col-sm-6 m-0">&copy; 2024 Andrés Gutiérrez.</p>
+        <ul class="d-flex grid gap-4 align-items-center m-0 col-12 col-sm-6 justify-content-sm-end" style="list-style-type: none;">
+            <li class="dashed">
+                <a href="#sobre-mi" class="dashed">Sobre mí</a> 
+            </li>
+            <li>
+                <a href="mailto:andres52885241@gmail.com" target="_blank" class="dashed">Contacto</a>
+            </li>
+        </ul>
+    </div>
+    
+</footer>
+
+</body>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script>
+$(document).ready(function () {
+    const BtnR  = $('.btn-r');
+    const BtnL = $('.btn-l');
+    const scrolla = $('.cartas-scroll');
+    var navbar = $(".navbar-nav");
+
+    BtnR.on("click", function () {
+        scrolla.animate({
+            scrollLeft: "+=300"
+        }, "slow");
+        toggleButtonsVisibility();
+    });
+    
+    BtnL.on("click", function () {
+        scrolla.animate({
+            scrollLeft: "-=300" 
+        }, "slow");
+        toggleButtonsVisibility();
+    });
+
+    function checkScroll() {
+        if ($(window).scrollTop() > 10) {
+            navbar.addClass("scrolled");
+        } else {
+            navbar.removeClass("scrolled");
+        }
+    }
+
+    checkScroll();
+    
+    $(window).scroll(checkScroll);
+});
+</script>
+
+</body>
